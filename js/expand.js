03d068f6d79e8b1ae05111ad8c7b4b1eba99c249
Alyssa Hudon <piningpianist@gmail.com>
2014-12-01 14:27:01 -0800

    changed index css

---
+++ js/expand.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alyssa on 11/30/2014.
+ */
+
+$(document).ready(function() {
+    $('.expandable').click(function() {
+        this.setAttribute('display', 'inline-block');
+    })
+})
