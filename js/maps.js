d9da9b77c6dc57d23078bc1f9d2502ce5386b94a
Alyssa Hudon <piningpianist@gmail.com>
2014-12-21 10:56:45 -0800

    mobile version for home page

--- js/maps.js
+++ js/maps.js
@@ -2,16 +2,22 @@
 "use strict";
 
 $( document ).ready(function() {
-    var markers = [];
+
+    var center = new google.maps.LatLng(36.5215598,-6.2781756);
 
     var mapOptions = {
-        center: { lat: 36.5215598, lng: -6.2781756},
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        center: center,
+        zoom: 5,
+        scrollwheel: false
     };
-
     //initialize the map
     var map = new google.maps.Map(document.getElementById('map'),
         mapOptions);
+
+    var cadizMarker = new google.maps.Marker({
+       position: center,
+        map: map,
+        title: 'Cádiz'
+    });
 
 });
